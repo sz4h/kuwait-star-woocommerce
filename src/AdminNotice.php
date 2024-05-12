b826a9c971c32310aba66282fa844aaf6a1562be
Ahmed Safaa <ahmed@sz4h.com>
2024-05-12 11:35:37 +0300

    Fix Credit

--- src/AdminNotice.php
+++ src/AdminNotice.php
@@ -16,20 +16,21 @@ class AdminNotice {
 		if ( ! @$options['alert_threshold'] ) {
 			return;
 		}
-		if ( ! $credit ) {
-			$credit = kuwait_star_api()->credit();
+		if ( ! $credit && @$options['email'] && @$options['password'] && @$options['domain'] ) {
+			$credit = kuwait_star_api()?->credit();
+
 			set_transient( 'kuwait_star_daily_credit', $credit, 60 * 60 * 24 );
 		}
-		if ($credit <= $options['alert_threshold']) {
+		if ( $credit && $credit <= $options['alert_threshold'] ) {
 			$this->credit = $credit;
-			add_action( 'admin_notices',[$this,'credit_low'] );
+			add_action( 'admin_notices', [ $this, 'credit_low' ] );
 		}
 
 	}
 
 	public function credit_low(): void {
-		$class = 'notice notice-error';
-		$message = sprintf( __( 'Kuwait Star credit low (Current credit: %s)', SPWKS_TD ), $this->credit);
+		$class   = 'notice notice-error';
+		$message = sprintf( __( 'Kuwait Star credit low (Current credit: %s)', SPWKS_TD ), $this->credit );
 
 		printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) );
 	}
